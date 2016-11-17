706246941958d124b18769601557d916833266d7
Bart Verwilst <bart@intux.be>
2016-11-17 12:47:48 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,5 +23,12 @@
     </script>
 
     <script src="/js/app.js"></script>
+    
+    @if ( Config::get('app.debug') )
+  <script type="text/javascript">
+      document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
+  </script>
+    @endif
+    
     </body>
 </html>
